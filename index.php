b9e2e136e83674599699706ee3c6672866e20b67
VarunIrani <varunirani0505@gmail.com>
2020-01-06 00:50:09 +0530

    Fixed home page gallery carousel

--- index.php
+++ index.php
@@ -203,73 +203,80 @@ require "header.php";
 							<div class="text-center mb-3">
 								<h2 class="text-black">GALLERY</h2>
 							</div>
-							<div class="col-lg-12">
-								<div>
-									<div
-										id="carouselExampleIndicators"
-										class="carousel slide"
-										data-ride="carousel"
-									>
-										<ol class="carousel-indicators">
-											<li
-												data-target="#carouselExampleIndicators"
-												data-slide-to="0"
-												class="active"
-											></li>
-											<li
-												data-target="#carouselExampleIndicators"
-												data-slide-to="1"
-											></li>
-											<li
-												data-target="#carouselExampleIndicators"
-												data-slide-to="2"
-											></li>
-										</ol>
-										<div class="carousel-inner">
-											<div class="carousel-item active">
-												<img
-													src="assets/img/1.jpg"
-													class="d-block w-100"
-													alt="..."
-												/>
-											</div>
-											<div class="carousel-item">
-												<img
-													src="assets/img/2.jpg"
-													class="d-block w-100"
-													alt="..."
-												/>
-											</div>
-											<div class="carousel-item">
-												<img
-													src="assets/img/3.jpg"
-													class="d-block w-100"
-													alt="..."
-												/>
-											</div>
+							<div class="col-md-12">
+								<div
+									id="carouselExampleIndicators"
+									class="carousel slide"
+									data-ride="carousel"
+								>
+									<ol class="carousel-indicators">
+										<li
+											data-target="#carouselExampleIndicators"
+											data-slide-to="0"
+											class="active"
+										></li>
+										<li
+											data-target="#carouselExampleIndicators"
+											data-slide-to="1"
+										></li>
+										<li
+											data-target="#carouselExampleIndicators"
+											data-slide-to="2"
+										></li>
+									</ol>
+									<div class="carousel-inner">
+										<div class="carousel-item active" style="height: 30em;">
+											<img
+												src="assets/img/1.jpg"
+												class="d-block ml-auto mr-auto"
+												alt="..."
+												style="height: 30em; width: fit-content;"
+											/>
 										</div>
-										<a
-											class="carousel-control-prev"
-											href="#carouselExampleIndicators"
-											role="button"
-											data-slide="prev"
-											><span
-												class="carousel-control-prev-icon"
-												aria-hidden="true"
-											></span>
-											<span class="sr-only">Previous</span> </a
-										><a
-											class="carousel-control-next"
-											href="#carouselExampleIndicators"
-											role="button"
-											data-slide="next"
-											><span
-												class="carousel-control-next-icon"
-												aria-hidden="true"
-											></span>
-											<span class="sr-only">Next</span></a
+										<div class="carousel-item" style="height: 30em;">
+											<img
+												src="assets/img/2.jpg"
+												class="d-block ml-auto mr-auto"
+												alt="..."
+												style="height: 30em; width: fit-content;"
+											/>
+										</div>
+										<div
+											class="carousel-item text-center"
+											style="height: 30em;"
 										>
+											<img
+												src="assets/img/MESCOE.jpg"
+												class="d-block ml-auto mr-auto"
+												alt="..."
+												style="height: 30em;"
+											/>
+										</div>
 									</div>
+									<a
+										class="carousel-control-prev"
+										href="#carouselExampleIndicators"
+										role="button"
+										data-slide="prev"
+									>
+										<span
+											class="carousel-control-prev-icon"
+											aria-hidden="true"
+										></span>
+										<span class="sr-only">Previous</span>
+									</a>
+									<a
+										class="carousel-control-next"
+										href="#carouselExampleIndicators"
+										role="button"
+										data-slide="next"
+									>
+										<span
+											class="carousel-control-next-icon"
+											aria-hidden="true"
+										></span>
+										<span class="sr-only">Next</span>
+									</a>
 								</div>
 							</div>
 						</div>
